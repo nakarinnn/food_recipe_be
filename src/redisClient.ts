b084804cb14f7b0468fc216f02b77911b0ed971e
nakarinnn <namekung951.15999@gmail.com>
2025-03-04 03:25:14 +0700

    chang host redis

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,15 +1,15 @@
 import Redis from 'ioredis';
 
 // กำหนดค่าการเชื่อมต่อ Redis
-// const redis = new Redis({
-//   host: process.env.REDIS_HOST,
-//   port: parseInt(process.env.REDIS_PORT || '6379'),
-//   username: process.env.REDIS_USERNAME,
-//   password: process.env.REDIS_PASSWORD,
-//   tls: { rejectUnauthorized: false }
-// });
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT || '6379'),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  tls: { rejectUnauthorized: false }
+});
 
-const redis = new Redis(process.env.REDIS_url as string);
+// const redis = new Redis(process.env.REDIS_url as string);
 
 
 // กำหนดค่าการเชื่อมต่อ Redis
