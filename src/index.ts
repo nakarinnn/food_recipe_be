4d69adb262cbf784ea2ac8b360c307b7df17134f
nakarinnn <namekung951.15999@gmail.com>
2025-03-27 19:42:44 +0700

    add orgin url

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ["https://food-recipe-fe-five.vercel.app/"],
+  origin: ["https://food-recipe-fe.onrender.com"],
   // origin: [process.env.ORIGIN_URL as string|| "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
