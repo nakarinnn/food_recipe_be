922ef5ee2f2a207c5ba313a831a46a95d91b00fb
nakarinnn <namekung951.15999@gmail.com>
2025-03-27 17:59:46 +0700

    fix code

--- src/services/LikeService.ts
+++ src/services/LikeService.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Like from "../models/likeModel";
 
 export const likeItem = async (userId: string, targetId: string, targetType: "Food" | "Comment") => {
@@ -16,9 +17,52 @@ export const getUserLikes = async (userId: string) => {
     return await Like.find({ user: userId })
 }
 
-export const getFavoriteMenu = async (userId: string) => {
-    const favmenu = await Like.find({ user: userId, targetType: 'Food' }).populate('targetId').sort({ createdAt: -1 })
-    const filteredFavMenu = favmenu.filter(item => item.targetId !== null);
 
-    return filteredFavMenu
+export const getFavoriteMenu = async (userId: string) => {
+    try {
+        const likeWithAvg = await Like.aggregate([
+            {
+                $match: { user: new mongoose.Types.ObjectId(userId), targetType: "Food" },
+            },
+            {
+                $lookup: {
+                    from: "foods",
+                    localField: "targetId",
+                    foreignField: "_id",
+                    as: "new_targetId",
+                },
+            },
+            { $unwind: "$new_targetId" },
+            {
+                $lookup: {
+                    from: "rating",
+                    localField: "new_targetId.ratings",
+                    foreignField: "_id",
+                    as: "new_targetId.ratingDetails",
+                },
+            },
+            { $sort: { createdAt: -1 } },
+        ]);
+
+        const likeWithAvgDetail = likeWithAvg.map((item) => ({
+            _id: item.new_targetId._id,
+            name: item.new_targetId.name,
+            description: item.new_targetId.description,
+            image: item.new_targetId.image,
+            cookTime: item.new_targetId.cookTime,
+            ratings: item.new_targetId.ratingDetails || [],
+        }));
+
+        const filteredFavMenu = likeWithAvgDetail.map((l: any) => ({
+            ...l,
+            avg: l.ratings.length
+                ? l.ratings.reduce((sum: number, r: any) => sum + (r.rating || 0), 0) / l.ratings.length
+                : 0,
+        }));
+        
+        return filteredFavMenu;
+    } catch (err) {
+        console.log(err);
+        throw new Error("Error fetching favorite menu");
+    }
 };
